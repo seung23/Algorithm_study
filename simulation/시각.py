32bf5beab151601caf9dfef96be0582003d3281d
seung23 <qortmd6471@gmail.com>
2026-03-19 18:29:02 +0900

    as

---
+++ simulation/시각.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if "3" in f"{i}{j}{k}":
+                count += 1
+
+print(count)
