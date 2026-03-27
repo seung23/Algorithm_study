8828e2e18df41fb07995bd6a61c0686661817138
seung23 <qortmd6471@gmail.com>
2026-03-27 15:31:21 +0900

    rotate template

---
+++ 빈출코드템플릿/정사각형회전(인덱스규칙).py
@@ -0,0 +1,37 @@
+### 정사각형 ###
+n = 3 #(3x3 정사각형)
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+for a in arr:
+    print(*a)
+print("___________\n")
+
+arr_90 = [[0]*n for _ in range(n)]
+for i in range(n): 
+    for j in range(n): 
+        arr_90[j][n-1-i] = arr[i][j] # (n-1) = max_r (0-indexed에서 마지막 인덱스 2)
+
+for a in arr_90:
+    print(*a)
+print("___________\n")
+
+arr_180 = [[0]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        arr_180[n-1-i][n-1-j] = arr[i][j]
+
+for a in arr_180:
+    print(*a)
+print("___________\n")
+
+arr_270 = [[0]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        arr_270[n-1-j][i] = arr[i][j]
+
+for a in arr_270:
+    print(*a)
+print("___________\n")
+
+
+
---
+++ 빈출코드템플릿/직사각형회전(인덱스규칙).py
@@ -0,0 +1,47 @@
+### 직사각형 ###
+arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12]] # n, m = 3, 4
+
+def rotated_90(arr): 
+    n = len(arr) 
+    m = len(arr[0]) 
+    result = [[0]*n for _ in range(m)] # 행 <-> 열 뒤집힘 주의(4x3)
+    for i in range(n): 
+        for j in range(m): 
+            result[j][n-1-i] = arr[i][j]
+    return result
+
+def rotated_180(arr): 
+    n = len(arr) 
+    m = len(arr[0]) 
+    result = [[0]*m for _ in range(n)] # 원래대로(3x4)
+    for i in range(n): 
+        for j in range(m): 
+            result[n-1-i][m-1-j] = arr[i][j]
+    return result
+
+
+def rotated_270(arr):
+    n = len(arr)
+    m = len(arr[0])
+    result = [[0]*n for _ in range(m)] # 행 <-> 열 뒤집힘 주의(4x3)
+    for i in range(n):
+        for j in range(m):
+            result[m-1-j][i] = arr[i][j]
+    return result
+
+
+for a in arr:
+    print(*a)
+print("___________\n")
+
+a90 = rotated_90(arr)
+for a in a90:
+    print(*a)
+print("___________\n")  
+a180 = rotated_180(arr)
+for a in a180:
+    print(*a)
+print("___________\n")    
+a270 = rotated_270(arr)
+for a in a270:
+    print(*a)
